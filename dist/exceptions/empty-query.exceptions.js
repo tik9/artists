18e13cfb2dc96c542b5af13a9299855a1eaf3938
ti ko <thome@tis-Mac>
2023-02-15 23:36:26 +0100

    first

--- dist/exceptions/empty-query.exceptions.js
+++
@@ -1,6 +0,0 @@
-export class EmptyQueryException extends Error {
-    constructor(msg) {
-        super(msg);
-        this.name = 'EmptyQueryError';
-    }
-}
--- dist/validators/query.validator.js
+++
@@ -1,7 +0,0 @@
-import { EmptyQueryException } from "../exceptions/empty-query.exceptions";
-const validate = (str) => {
-    if (!str) {
-        throw new EmptyQueryException('Parameter required');
-    }
-};
-export default validate;
